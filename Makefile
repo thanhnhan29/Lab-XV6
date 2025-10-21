05988c99aa2a6bb32ee3d9fc3600bd7a12377141
thanhnhan29 <vothanhnhanaglx@gmail.com>
2025-10-21 20:35:52 +0700

    add find command implementation and update Makefile

--- Makefile
+++ Makefile
@@ -197,6 +197,7 @@ UPROGS=\
 	$U/_sleep\
 	$U/_primes\
 	$U/_pingpong\
+	$U/_find\
 
 
 
---
+++ user/find.c
@@ -0,0 +1,75 @@
+﻿#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+#include "kernel/fcntl.h"
+
+char*
+fmtname(char *path)
+{
+  char *p;
+  for(p=path+strlen(path); p >= path && *p != '/'; p--)
+    ;
+  p++;
+
+  return p;
+}
+
+void
+find(char *path, char *findname)
+{
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+
+  if((fd = open(path, O_RDONLY)) < 0){
+    fprintf(2, "ls: cannot open %s\n", path);
+    return;
+  }
+
+  if(fstat(fd, &st) < 0){
+    fprintf(2, "ls: cannot stat %s\n", path);
+    close(fd);
+    return;
+  }
+
+  switch(st.type){
+  case T_DEVICE:
+  case T_FILE:
+    if(strcmp(fmtname(path), findname)==0)  printf("%s\n", path);
+    break;
+
+  case T_DIR:
+    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+      printf("ls: path too long\n");
+      break;
+    }
+    strcpy(buf, path);
+    p = buf+strlen(buf);
+    *p++ = '/';
+    while(read(fd, &de, sizeof(de)) == sizeof(de)){
+      if(de.inum == 0)
+        continue;
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0;
+      if(stat(buf, &st) < 0){
+        printf("ls: cannot stat %s\n", buf);
+        continue;
+      }
+    //   printf("%s %d %d %d\n", fmtname(buf), st.type, st.ino, (int) st.size);
+      if(strcmp(de.name,".")==0) continue;
+      if(strcmp(de.name,"..")==0) continue;
+      find(buf, findname);
+    }
+    break;
+  }
+  close(fd);
+}
+
+int
+main(int argc, char *argv[])
+{
+  find(argv[1], argv[2]);
+  exit(0);
+}
